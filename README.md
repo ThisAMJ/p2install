3239102d08ef52804aa1dcc6fcffe3b21529c210
Jesse <69196954+ThisAMJ@users.noreply.github.com>
2023-07-18 16:36:15 +1000

    wip: initial stuff upload
    windows thing is missing some things until i have effort

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # p2install
+
+The idea of this repo is to generalise the speedrunning install process for Portal 2 and its mods as much as possible.
+
+Once the launch option is added, almost everything will be automatically installed.
+
+Credit goes to mlugg for the original Linux script this has been adapted from.
+
+## Launch options
+
+WIP: in time this may come down to just one launch option,
+
+```txt
+[path to]/.p2-common/portal2.bat %command%
+[path to]/.p2-common/portal2.sh %command%
+```
+
+I even wonder if there's some way to have a single file that works on both Windows and Linux.
+
+| Game | Launch options<br>Windows | Launch options<br>Linux
+| ---- | -------------- | --------------
+| Portal 2 | [path to]/.p2-common/portal2.bat portal2 %command%                                                                                             | [path to]/.p2-common/portal2.sh portal2 %command%
+| PS:Mel   | [path to]/.p2-common/portal2.bat portal_stories %command%                                                                                      | [path to]/.p2-common/portal2.sh portal_stories %command%
+| SRM      | [path to]/.p2-common/portal2.bat "..\\..\\sourcemods\\Portal 2 Speedrun Mod" %command%<br>*Change path if Speedrun Mod is on a different drive*| [path to]/.p2-common/portal2.sh "../../sourcemods/Portal 2 Speedrun Mod" %command%<br>*Change path if Speedrun Mod is on a different drive*
+| Reloaded | [path to]/.p2-common/portal2.bat portal2 %command%                                                                                             | [path to]/.p2-common/portal2.sh portal2 %command%
+| Aptag    | [path to]/.p2-common/portal2.bat aperturetag %command%                                                                                         | [path to]/.p2-common/portal2.sh aperturetag %command%<br>*The use of Proton is recommended.*
+| TWTM     | [path to]/.p2-common/portal2.bat TWTM %command%                                                                                                | [path to]/.p2-common/portal2.sh TWTM %command%<br>*The use of Proton is recommended.*
---
+++ portal2.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+clear
+
+COMMONDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+GAMEROOT="$PWD"
+
+GAMEPATH="$1"
+GAME=$(basename "$PWD")
+if [[ "$GAMEPATH" =~ Portal\ 2\ Speedrun\ Mod$ ]]; then
+    GAME="Portal 2 Speedrun Mod"
+fi
+
+shift
+
+STEAM_RT32="$HOME/.local/share/Steam/ubuntu12_32/steam-runtime"
+# STEAM_RT64="$HOME/.local/share/Steam/ubuntu12_64/steam-runtime"
+
+STEAM_RT="$STEAM_RT32"
+# if [[ "$GAME" == "Portal Reloaded" ]]; then
+#     STEAM_RT="$STEAM_RT64"
+# fi
+
+REAPER="$STEAM_RT32/../reaper"
+echo ""
+echo STEAM
+echo $STEAM_RT
+echo $GAMEROOT
+echo $COMMONDIR
+echo $GAMEPATH
+echo $GAME
+
+export LD_LIBRARY_PATH=""
+export LD_LIBRARY_PATH="$GAMEROOT/bin/linux32:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$GAMEROOT/bin:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$STEAM_RT32/pinned_libs_32:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$STEAM_RT/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$STEAM_RT/usr/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$STEAM_RT/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$STEAM_RT/usr/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib32:$LD_LIBRARY_PATH"
+echo ""
+echo LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+
+export LD_PRELOAD=""
+export LD_PRELOAD="$STEAM_RT/../gameoverlayrenderer.so:$LD_PRELOAD"
+
+echo ""
+echo LD_PRELOAD
+echo $LD_PRELOAD
+
+GAMEEXE=portal2
+
+EXTRA_ARGS="-novid +mat_motion_blur_enabled 0 -background 4 -condebug"
+
+# Vulkan seems to crash Mel, Reloaded, and Aptag upon launch
+if ! [[ "$GAME" == "Portal Stories Mel" || "$GAME" == "Portal Reloaded" || "$GAME" == "Aperture Tag" ]]; then
+    EXTRA_ARGS="$EXTRA_ARGS -vulkan"
+fi
+
+if [[ "$GAME" == "Portal Reloaded" ]]; then
+    EXTRA_ARGS="$EXTRA_ARGS -console"
+fi
+
+if [[ "$GAME" == "Thinking with Time Machine" ]]; then
+    GAMEEXE=TWTM
+fi
+
+echo ""
+echo ARGS
+echo $EXTRA_ARGS
+
+# Repair gameinfo with p2common
+if [[ -f "$COMMONDIR/.util/gameinfo/$GAME.txt" ]]; then
+    echo "Installing p2common..."
+    ESCAPED_GAMEROOT=$(printf '%s\n' "$GAMEROOT" | sed -e 's/[\/&]/\\&/g')
+    ESCAPED_COMMONDIR=$(printf '%s\n' "$COMMONDIR" | sed -e 's/[\/&]/\\&/g')
+    cp -f  "$COMMONDIR/.util/gameinfo/$GAME.txt"     "$GAMEROOT/$GAMEPATH/gameinfo.txt"
+    sed -i "s/GAMEROOTGOESHERE/$ESCAPED_GAMEROOT/"   "$GAMEROOT/$GAMEPATH/gameinfo.txt"
+    sed -i "s/COMMONDIRGOESHERE/$ESCAPED_COMMONDIR/" "$GAMEROOT/$GAMEPATH/gameinfo.txt"
+fi
+
+# Remove Portal Reloaded's default autoexec.cfg if we have a common one
+if [[ "$GAME" == "Portal Reloaded" ]]; then
+    if [[ -f "$COMMONDIR/cfg/autoexec.cfg" ]]; then
+        if [[ -f "$GAMEROOT/portalreloaded/cfg/autoexec.cfg" ]]; then
+            if cmp --silent -- "$GAMEROOT/portalreloaded/cfg/autoexec.cfg" "$COMMONDIR/.util/reloaded-original-autoexec1.cfg" ||
+            cmp --silent -- "$GAMEROOT/portalreloaded/cfg/autoexec.cfg" "$COMMONDIR/.util/reloaded-original-autoexec2.cfg"; then
+                echo "Removing Portal Reloaded default autoexec.cfg ($GAMEROOT/portalreloaded/cfg/autoexec.cfg)..."
+                rm "$GAMEROOT/portalreloaded/cfg/autoexec.cfg"
+            fi
+        fi
+    fi
+fi
+
+# Pack VPKs for DLCs
+highest_dlc=3 # TODO: Is this different for any games?
+while [[ -d "portal2_dlc$highest_dlc" ]]; do
+    cd "portal2_dlc$highest_dlc"
+    if [[ -d ".root" ]]; then
+        echo ""
+        echo "VPK packing portal2_dlc$highest_dlc/.root"
+        zvpk .root/
+    fi
+    cd ..
+    ((highest_dlc++))
+done
+
+GAMEEXE+=_linux
+
+ulimit -n 2048
+
+cd "$GAMEROOT"
+
+STATUS=42
+while [ $STATUS -eq 42 ]; do
+    if [ "$GAME_DEBUGGER" == "gdb" ]; then
+        ARGSFILE="$(mktemp /tmp/portal2.gdb.XXXX)"
+        echo "run $@ -steam $EXTRA_ARGS -game \"$GAMEPATH\"" > "$ARGSFILE"
+        echo show args >> "$ARGSFILE"
+        "$REAPER" -- $GAME_DEBUGGER "$GAMEROOT/$GAMEEXE" -x "$ARGSFILE"
+        rm "$ARGSFILE"
+    else
+        echo ""
+        echo "ALLARGS"
+        echo "\"$REAPER\" -- $GAME_DEBUGGER \"$GAMEROOT/$GAMEEXE\" \"\" -steam $EXTRA_ARGS -game \"$GAMEPATH\""
+        echo ""
+        "$REAPER" -- $GAME_DEBUGGER "$GAMEROOT/$GAMEEXE" "" -steam $EXTRA_ARGS -game "$GAMEPATH"
+    fi
+	STATUS=$?
+done
+exit $STATUS
