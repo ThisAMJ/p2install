3387f4add0f2d86806cc3e42596815015723ba3e
Jesse <69196954+ThisAMJ@users.noreply.github.com>
2023-07-18 22:13:47 +1000

    refactor: hide things

--- portal2.bat
+++ portal2.bat
@@ -3,9 +3,9 @@
 GOTO :CMDSCRIPT
 ::CMDLITERAL
 clear
-/bin/bash $(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/portal2-linux.sh "$@"
+/bin/bash $(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.portal2-linux.sh "$@"
 exit $?
 :CMDSCRIPT
 CLS
-CALL "%~dp0portal2-windows.bat" %*
+CALL "%~dp0.portal2-windows.bat" %*
 EXIT /B
