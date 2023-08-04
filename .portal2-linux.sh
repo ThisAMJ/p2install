608d2d2c183e028cbe6d6908138ce538fc18cd34
Jesse <69196954+ThisAMJ@users.noreply.github.com>
2023-08-04 11:31:33 +1000

    wip: windows support sh
    wip because i need a portable bash shell (that maybe has a good 'ln'? git bash seems to just copy the files) or change to a different language (python?)
    with git bash it also seems to run really slowly (not counting the ln stupidity) (gameinfo copy/reading maybe)

--- .portal2-linux.sh
+++ .portal2-linux.sh
@@ -4,25 +4,47 @@ clear
 COMMONDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 GAMEROOT="$PWD"
 GAMEPATH="$GAMEROOT"
-GAMEEXE="$7"
 GAMENAME=$(basename "$PWD")
 GAMENAME_RAW=$GAMENAME
 GAMENAMEROOT_RAW="$GAMEROOT"
 
+echo $(date) >> "$COMMONDIR/p2install.log"
+
+WINDOWS=0
 LINUX=0
 MACOSX=0
-UNAME=`uname`
-if [ "$UNAME" == "Darwin" ]; then
-	MACOSX=1
-elif [ "$UNAME" == "Linux" ]; then
-	LINUX=1
-fi
-
 PROTON=0
+PLATFORM=`uname`
+if   [[ "$PLATFORM" == *"Darwin"* ]]; then
+	PLATFORM="MacOSX"
+	MACOSX=1
+	echo "MacOSX not yet supported. Sorry!" >> "$COMMONDIR/p2install.log"
+	echo "In launch of: $@" >> "$COMMONDIR/p2install.log"
+	echo "" >> "$COMMONDIR/p2install.log"
+	exit 1
+elif [[ "$PLATFORM" == *"Linux"* ]]; then
+	PLATFORM="Linux"
+	LINUX=1
+	GAMEEXE="$7"
+elif [[ "$PLATFORM" == *"Windows"* || "$PLATFORM" == *"NT"* ]]; then
+	PLATFORM="Windows"
+	WINDOWS=1
+	GAMEEXE="$1"
+	HOME=$USERPROFILE
+fi
 if [[ "$7" == *"/common/SteamLinuxRuntime"* ]]; then
 	PROTON=1
 	GAMEEXE="${12}"
 fi
+
+if [[ ! -f "$GAMEEXE" ]]; then
+	echo "Malformed launch command: $@" >> "$COMMONDIR/p2install.log"
+	echo "Launch option should be in the form of:" >> "$COMMONDIR/p2install.log"
+	echo "$COMMONDIR/portal2.bat %command%" >> "$COMMONDIR/p2install.log"
+	echo "" >> "$COMMONDIR/p2install.log"
+	exit 1
+fi
+
 GAMEEXE=$(basename "$GAMEEXE")
 
 STEAM="$HOME/.steam/steam"
@@ -30,38 +52,33 @@ if [[ -f "$COMMONDIR/steam-location.txt" ]]; then
 	STEAM="$(cat "$COMMONDIR/steam-location.txt")"
 fi
 if ! [[ -d "$STEAM" ]]; then STEAM="$HOME/.local/share/Steam"; fi
+if ! [[ -d "$STEAM" ]]; then STEAM="$(printenv "ProgramFiles(x86)")/Steam"; fi
 if ! [[ -d "$STEAM" ]]; then
 	echo ERROR! STEAM NOT FOUND! >> "$COMMONDIR/p2install.log"
 	echo In launch of: $@ >> "$COMMONDIR/p2install.log"
-	echo Put the path to Steam in steam-location.txt >> "$COMMONDIR/p2install.log"
+	echo "Put the path to Steam in steam-location.txt (or yell at AMJ idk)" >> "$COMMONDIR/p2install.log"
 	echo "" >> "$COMMONDIR/p2install.log"
 	exit 1
 fi
-STEAM_RT="$STEAM/ubuntu12_32/steam-runtime"
-REAPER="$STEAM_RT/../reaper"
-if ! [[ -f "$REAPER" ]]; then REAPER="$1"; fi
-if ! [[ -f "$REAPER" ]]; then
-	echo ERROR! REAPER NOT FOUND AT $REAPER! >> "$COMMONDIR/p2install.log"
-	echo In launch of: $@ >> "$COMMONDIR/p2install.log"
-	echo "" >> "$COMMONDIR/p2install.log"
-	exit 1
-fi
-
 
 GAMEARG="portal2"
-EXTRA_ARGS="-novid +mat_motion_blur_enabled 0 -background 5 -condebug -console" # -vulkan
+EXTRA_ARGS="-novid +mat_motion_blur_enabled 0 -background 5 -condebug -console"
 if [[ -f "$COMMONDIR/extra-args.txt" ]]; then
 	EXTRA_ARGS="$(cat "$COMMONDIR/extra-args.txt")"
 fi
+
 # Vulkan seems to crash Mel, Reloaded, and Aptag upon launch
-if ! [[ "$GAMENAME" == "Portal Stories Mel" || "$GAMENAME" == "Portal Reloaded" || "$GAMENAME" == "Aperture Tag" ]]; then
-	EXTRA_ARGS="$EXTRA_ARGS -vulkan"
+VULKAN=1
+if [[ "$LINUX" -eq 1 ]]; then
+	if [[ "$GAMENAME" == "Portal Stories Mel" || "$GAMENAME" == "Portal Reloaded" || "$GAMENAME" == "Aperture Tag" ]]; then VULKAN=0; fi
 fi
+if [[ "$VULKAN" -eq 1 ]]; then EXTRA_ARGS="$EXTRA_ARGS -vulkan"; fi
+
 ARGIDX=1
 for var in "$@"; do
 	if [[ "$var" == "-game" ]]; then
 		GAMENEXT=1
-	elif [[ "$GAMENEXT" == 1 ]]; then
+	elif [[ "$GAMENEXT" -eq 1 ]]; then
 		GAMEARG="$var"
 		GAMENEXT=0
 		if [[ "$GAMEARG" == *"/sourcemods/"* ]]; then
@@ -70,20 +87,20 @@ for var in "$@"; do
 			GAMEPATH="$GAMEARG"
 		fi
 	else
-		if [[ "$PROTON" == 1 ]]; then
-			if [[ "$ARGIDX" -gt 12 ]]; then EXTRA_ARGS+=" $var"; fi
-		else
-			if [[ "$ARGIDX" -gt 7 ]]; then EXTRA_ARGS+=" $var"; fi
-		fi
+		ARG_COMPARE=0
+		if [[ "$WINDOWS"  -eq 1 ]]; then ARG_COMPARE=1
+		elif [[ "$PROTON" -eq 1 ]]; then ARG_COMPARE=12
+		elif [[ "$LINUX"  -eq 1 ]]; then ARG_COMPARE=7; fi
+		if [[ "$ARGIDX" -gt "$ARG_COMPARE" ]]; then EXTRA_ARGS="$EXTRA_ARGS $var"; fi
 	fi
 	((ARGIDX++))
 done
 
 # If GAMEEXE ends with .sh, replace it with our platform
 if [[ "$GAMEEXE" == *".sh" ]]; then
-	if [[ "$LINUX" == 1 ]]; then
+	if [[ "$LINUX" -eq 1 ]]; then
 		GAMEEXE="${GAMEEXE::-3}_linux"
-	elif [[ "$MACOSX" == 1 ]]; then
+	elif [[ "$MACOSX" -eq 1 ]]; then
 		GAMEEXE="${GAMEEXE::-3}_osx"
 	fi
 fi
@@ -108,9 +125,9 @@ if ! [[ -d "$COMMONDIR/cfg" ]]; then mkdir "$COMMONDIR/cfg"; fi
 # Remove any cfgs that are common from the game's cfg folder(s)
 MAIN_DIR=""
 while read line || [[ -n $line ]]; do
-	line=$(echo "$line" | sed -e "s/|gameinfo_path|/$GAMEARG\//")
 	if [[ "$line" != $(echo "$line" | sed -e 's/^Game[ \t][ \t"]*\([^"\r\n]*\).*$/\1/') ]]; then
 		line=$(echo "$line" | sed -e 's/^Game[ \t][ \t"]*\([^"\r\n]*\).*$/\1/')/
+		line=$(echo "$line" | sed -e "s/|gameinfo_path|/$GAMEARG\//")
 		line=$(echo "$line" | sed -e 's/\/\.\//\//g')
 		line=$(cd "$line" && pwd)
 		if [[ "$MAIN_DIR" == "" ]]; then MAIN_DIR="$line"; fi
@@ -126,6 +143,7 @@ while read line || [[ -n $line ]]; do
 done < "$GAMEARG/gameinfo.txt"
 
 # Make symlinks to the game's directories for convenience and install things
+# Debatable whether svars should be common
 mkdir -p "$COMMONDIR/.dirs"; rm -f "$COMMONDIR/.dirs/$GAMENAME"; ln -s "$GAMEPATH" "$COMMONDIR/.dirs/$GAMENAME"
 mkdir -p "$COMMONDIR/.tas/$GAMENAME_RAW"
 mkdir -p "$COMMONDIR/.crash_reports"
@@ -139,11 +157,12 @@ if [[ -f "$MAIN_DIR/console.log" ]];   then rm -f "$MAIN_DIR/console.log"; fi; l
 if [[ -f "$GAMEROOT/svars_persist" ]]; then mv -f "$GAMEROOT/svars_persist" "$COMMONDIR"; fi; ln -s "$COMMONDIR/svars_persist" "$GAMEROOT/svars_persist"
 
 # platform.cfg
-echo "svar_set gameplatform $UNAME"     > "$COMMONDIR/cfg/platform.cfg"
-echo "svar_set linux $LINUX"           >> "$COMMONDIR/cfg/platform.cfg"
-echo "svar_set windows 0"              >> "$COMMONDIR/cfg/platform.cfg"
-echo "svar_set macos $MACOSX"          >> "$COMMONDIR/cfg/platform.cfg"
-echo "svar_set gamearg \"$GAMEARG\""   >> "$COMMONDIR/cfg/platform.cfg"
+echo "svar_set gameplatform $PLATFORM"     > "$COMMONDIR/cfg/platform.cfg"
+echo "svar_set windows $WINDOWS"       >> "$COMMONDIR/cfg/platform.cfg"
+echo "svar_set linux   $LINUX"         >> "$COMMONDIR/cfg/platform.cfg"
+echo "svar_set proton  $PROTON"        >> "$COMMONDIR/cfg/platform.cfg"
+echo "svar_set macos   $MACOSX"        >> "$COMMONDIR/cfg/platform.cfg"
+echo "svar_set gamearg \"$(basename $GAMEARG)\""   >> "$COMMONDIR/cfg/platform.cfg"
 echo "svar_set gamename \"$GAMENAME\"" >> "$COMMONDIR/cfg/platform.cfg"
 
 # Pack VPKs for DLCs
@@ -159,25 +178,34 @@ done
 # Just copy into every steamid lol
 # Ditto for .util/maps (speedrun mods)
 
-LD_LIBRARY_PATH_INITIAL=$LD_LIBRARY_PATH
-LD_PRELOAD_INITIAL=$LD_PRELOAD
-if [[ "$PROTON" == 0 ]]; then
-	export LD_LIBRARY_PATH=""
-	export LD_LIBRARY_PATH="$GAMEROOT/bin/linux32:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="$GAMEROOT/bin:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="$STEAM_RT/pinned_libs_32:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="$STEAM_RT/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="$STEAM_RT/usr/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="$STEAM_RT/lib:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="$STEAM_RT/usr/lib:$LD_LIBRARY_PATH"
-	export LD_LIBRARY_PATH="/usr/lib32:$LD_LIBRARY_PATH"
-
-	export LD_PRELOAD=""
-	export LD_PRELOAD="$STEAM_RT/../gameoverlayrenderer.so:$LD_PRELOAD"
+if [[ "$LINUX" -eq 1 ]]; then
+	STEAM_RT="$STEAM/ubuntu12_32/steam-runtime"
+	REAPER="$STEAM_RT/../reaper"
+	if ! [[ -f "$REAPER" ]]; then REAPER="$1"; fi
+	if ! [[ -f "$REAPER" ]]; then
+		echo ERROR! REAPER NOT FOUND AT $REAPER! >> "$COMMONDIR/p2install.log"
+		echo In launch of: $@ >> "$COMMONDIR/p2install.log"
+		echo "" >> "$COMMONDIR/p2install.log"
+		exit 1
+	fi
+	LD_LIBRARY_PATH_INITIAL=$LD_LIBRARY_PATH
+	LD_PRELOAD_INITIAL=$LD_PRELOAD
+	if [[ "$PROTON" == 0 ]]; then
+		export LD_LIBRARY_PATH=""
+		export LD_LIBRARY_PATH="$GAMEROOT/bin/linux32:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="$GAMEROOT/bin:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="$STEAM_RT/pinned_libs_32:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="$STEAM_RT/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="$STEAM_RT/usr/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="$STEAM_RT/lib:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="$STEAM_RT/usr/lib:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="/usr/lib32:$LD_LIBRARY_PATH"
+		export LD_PRELOAD=""
+		export LD_PRELOAD="$STEAM_RT/../gameoverlayrenderer.so:$LD_PRELOAD"
+	fi
 fi
 
-echo $(date) >> "$COMMONDIR/p2install.log"
-echo "    PLATFORM: $UNAME" >> "$COMMONDIR/p2install.log"
+echo "    PLATFORM: $PLATFORM" >> "$COMMONDIR/p2install.log"
 echo "    GAMEROOT: $GAMEROOT (GAME $GAMEARG)" >> "$COMMONDIR/p2install.log"
 echo "     GAMEEXE: $GAMEEXE" >> "$COMMONDIR/p2install.log"
 echo "INITIAL_ARGS: $@" >> "$COMMONDIR/p2install.log"
@@ -186,17 +214,22 @@ echo "    GAMENAME: $GAMENAME (PROTON $PROTON)" >> "$COMMONDIR/p2install.log"
 echo "   COMMONDIR: $COMMONDIR" >> "$COMMONDIR/p2install.log"
 echo "" >> "$COMMONDIR/p2install.log"
 
-ulimit -n 2048
-
 cd "$GAMEROOT"
 
-STATUS=42
-while [ $STATUS -eq 42 ]; do
-	if [[ "$PROTON" == 1 ]]; then
-		"$REAPER" SteamLaunch "$3" -- "$5" -- "$7" --verb=waitforexitandrun -- "${10}" waitforexitandrun "$GAMEEXE" "" -game "$GAMEARG" $EXTRA_ARGS
-	else
-		"$REAPER" SteamLaunch "$3" -- $GAME_DEBUGGER "$GAMEROOT/$GAMEEXE" -game "$GAMEARG" $EXTRA_ARGS
-	fi
-	STATUS=$?
-done
-exit $STATUS
+if [[ "$WINDOWS" -eq 1 ]]; then
+	"$GAMEROOT/$GAMEEXE" -game "$GAMEARG" $EXTRA_ARGS
+	exit $ERRORLEVEL
+elif [[ "$LINUX" -eq 1 ]]; then
+	ulimit -n 2048 # Limit the game to at most 2048 files open at once (why?)
+
+	STATUS=42
+	while [ $STATUS -eq 42 ]; do
+		if [[ "$PROTON" == 1 ]]; then
+			"$REAPER" SteamLaunch "$3" -- "$5" -- "$7" --verb=waitforexitandrun -- "${10}" waitforexitandrun "$GAMEROOT/$GAMEEXE" "" -game "$GAMEARG" $EXTRA_ARGS
+		else
+			"$REAPER" SteamLaunch "$3" -- $GAME_DEBUGGER "$GAMEROOT/$GAMEEXE" -game "$GAMEARG" $EXTRA_ARGS
+		fi
+		STATUS=$?
+	done
+	exit $STATUS
+fi
--- portal2.bat
+++ portal2.bat
@@ -5,5 +5,6 @@ GOTO :CMDSCRIPT
 /bin/bash $(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.portal2-linux.sh "$@"
 exit $?
 :CMDSCRIPT
+@REM "%ProgramFiles%\Git\git-bash.exe" "%~dp0.portal2-linux.sh" %*
 CALL "%~dp0.portal2-windows.bat" %*
 EXIT /B
